9a619a71484fcd539bfca330f529f0f5b808fd91
Scott Cytacki <scytacki@concord.org>
2025-04-18 17:33:11 -0400

    NASAEARTH-11 CODAP slider updating map title

--- src/utils/codap-utils.ts
+++ src/utils/codap-utils.ts
@@ -1,7 +1,7 @@
 import { addDataContextChangeListener, initializePlugin, sendMessage } from "@concord-consortium/codap-plugin-api";
 import {
   kInitialDimensions, kMapName, kPinColorAttributeName, kPinDataContextName, kPinLatAttributeName,
-  kPinLongAttributeName, kPluginName, kVersion
+  kPinLongAttributeName, kPluginName, kSliderComponentName, kVersion
 } from "../data/constants";
 import { pluginState } from "../models/plugin-state";
 
@@ -24,14 +24,7 @@ export async function initializeNeoPlugin() {
   });
 
   // Create map if it doesn't exist
-  const existingMap = await sendMessage("get", `component[${kMapName}]`);
-  if (!existingMap.success) {
-    await sendMessage("create", "component", {
-      type: "map",
-      name: kMapName,
-      title: "Map"
-    });
-  }
+  await createOrUpdateMap("Map");
 
   // See if there are any existing pins
   pluginState.updatePins();
@@ -44,5 +37,60 @@ export async function initializeNeoPlugin() {
       pluginState.updatePins();
     }
   });
+
+}
+
+export async function createOrUpdateMap(title: string): Promise<void> {
+  const existingMap = await sendMessage("get", `component[${kMapName}]`);
+  if (!existingMap.success) {
+    await sendMessage("create", "component", {
+      type: "map",
+      name: kMapName,
+      title
+    });
+  } else {
+    await sendMessage("update", `component[${kMapName}]`, {
+      title
+      // When there is geo raster support in CODAP we also need to do something like this:
+      // geoRasterUrl: item.url,
+      //
+    });
+  }
+
+  // Note in the current geotiff branch it is necessary to call update after create
+  // because the create call ignores the geotiffUrl. Hopefully we can fix this in the updated
+  // CODAP implementation.
+}
+
+export async function createOrUpdateDateSlider(value: number, lowerBound:number, upperBound:number): Promise<void> {
+  const existingGlobal = await sendMessage("get", `global[Date]`);
+  if (!existingGlobal.success) {
+    await sendMessage("create", "global", {
+      name: "Date",
+      value,
+    });
+  } else {
+    await sendMessage("update", `global[Date]`, {
+      value
+    });
+  }
+
+  const existingSlider = await sendMessage("get", `component[${kSliderComponentName}]`);
+  if (!existingSlider.success) {
+    await sendMessage("create", "component", {
+      type: "slider",
+      title: kSliderComponentName,
+      globalValueName: "Date",
+      lowerBound,
+      upperBound,
+      // TODO: Not sure if this is needed
+      value
+    });
+  } else {
+    await sendMessage("update", `component[${kSliderComponentName}]`, {
+      lowerBound,
+      upperBound,
+    });
+  }
 
 }
