9d7537bb1d211e2faabb0a146e589b9a1854cd53
eireland <eireland@concord.org>
2025-05-12 12:24:17 -0700

    Switches geoname to use nearbyPlaces but added a sort to the returned places and use the largest population.

--- src/utils/location-utils.ts
+++ src/utils/location-utils.ts
@@ -23,7 +23,7 @@ export interface MapComponentInfo {
 
 const toRadians = (degrees: number) =>{
     return degrees * Math.PI / 180;
-}
+};
 
 const haversineDistance = (lat1: number, lon1: number, lat2: number, lon2: number) =>  {
     const R = 6371; // Earth radius in km
@@ -36,7 +36,7 @@ const haversineDistance = (lat1: number, lon1: number, lat2: number, lon2: numbe
 
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     return R * c;
-}
+};
 
 const findNearestCity = (targetLat: number, targetLon: number, citiesArray: any) => {
     let nearestCity = null;
@@ -51,22 +51,40 @@ const findNearestCity = (targetLat: number, targetLon: number, citiesArray: any)
     }
 
     return nearestCity;
-}
+};
 
 // Finds a geo name from lat/long
 export const geoLocSearch = async (lat: number, long: number, bounds: IBounds) => {
   const userClause = `username=${kGeonamesUser}`;
   const locClause = `lat=${lat}&lng=${long}`;
   const boundsClause = `north=${bounds.north}&south=${bounds.south}&east=${bounds.east}&west=${bounds.west}`;
-  const url = `${kGeolocCitiesService}?${[locClause, userClause, boundsClause].join("&")}`;
+  const maxRowsClause = `maxRows=20`;
+  const radiusClause = `radius=10`;
+  const populationLimitClause = "city15000";
+  // const url = `${kGeolocCitiesService}?${[locClause, userClause, boundsClause, maxRowsClause].join("&")}`;
+  const url = `${kGeolocService}?${[locClause, userClause, maxRowsClause, radiusClause, populationLimitClause ]
+                  .join("&")}`;
   try {
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
+      const geonames = data.geonames;
+      console.log("GeoLocSearch geonames", JSON.parse(JSON.stringify(geonames)));
+      console.log("data size", geonames.length);
+      const sortedGeonamesByPopulation = geonames.sort((a: any, b: any) => {
+        const populationA = parseInt(a.population, 10);
+        const populationB = parseInt(b.population, 10);
+        return populationB - populationA; // Sort in descending order
+      });
+      console.log("sorted geonames", JSON.parse(JSON.stringify(sortedGeonamesByPopulation)));
+
       // find the nearest location nearest the lat/long
-      const nearest = findNearestCity(lat, long, data.geonames);
+      // const nearest = findNearestCity(lat, long, data.geonames);
+      const nearest = sortedGeonamesByPopulation[0];
+      console.log("nearest location", JSON.parse(JSON.stringify(nearest)));
       return nearest
-        ? {success: true, values: {location:`${nearest.name}, ${nearest.countrycode}`}}
+        ? {success: true, values: {location:`${nearest.name}, ${nearest.adminCode1}`}}
+        // ? {success: true, values: {location:`${nearest.name}, ${nearest.countrycode}`}}
         : {success: false, values: {location: "Unknown Location"}};
     } else {
       return Promise.reject(response.statusText);
